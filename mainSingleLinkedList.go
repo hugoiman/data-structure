936ecd34a545fdc8b656d82c544305b6f22c90f3
Gen <hugogiman17@gmail.com>
2021-07-21 14:57:05 +0700

    First Commit: SingleLinkedList

--- mainSingleLinkedList.go
+++ mainSingleLinkedList.go
@@ -11,50 +11,51 @@ func main() {
 
 	fmt.Printf("Add Front: %d, --> ", 4)
 	list.AddFirst(4)
-	list.DisplayData()
+	list.DisplayData() // 4
 
 	fmt.Printf("Add Front: %d, --> ", 2)
 	list.AddFirst(2)
-	list.DisplayData()
+	list.DisplayData() // 2, 4
 
 	fmt.Printf("Add Front: %d, --> ", 6)
 	list.AddFirst(6)
-	list.DisplayData()
+	list.DisplayData() // 6, 2, 4
 
 	fmt.Printf("Add Last: %d, --> ", 9)
 	list.AddLast(9)
-	list.DisplayData()
+	list.DisplayData() // 6, 2, 4, 9
 
 	fmt.Printf("Add Last: %d, --> ", 19)
 	list.AddLast(19)
-	list.DisplayData()
+	list.DisplayData() // 6, 2, 4, 9, 19
 
 	fmt.Printf("Add %d After %d, --> ", 5, 4)
 	list.InsertAfter(5, 4)
-	list.DisplayData()
+	list.DisplayData() // 6, 2, 4, 5, 9, 19
 
 	fmt.Printf("Add %d After %d, --> ", 13, 11)
-	list.InsertAfter(13, 11)
-	list.DisplayData()
+	list.InsertAfter(13, 11) // Data 11 not found
+	list.DisplayData()       // 6, 2, 4, 5, 9, 19
 
 	fmt.Printf("Remove Front --> ")
 	list.RemoveFirst()
-	list.DisplayData()
+	list.DisplayData() // 2, 4, 5, 9, 19
 
 	fmt.Printf("Remove Last --> ")
 	list.RemoveLast()
-	list.DisplayData()
+	list.DisplayData() // 2, 4, 5, 9
 
 	fmt.Printf("Remove: %d, --> ", 4)
 	list.RemoveCertain(4)
-	list.DisplayData()
+	list.DisplayData() // 2, 5, 9
 
 	fmt.Printf("Remove: %d, --> ", 12)
-	list.RemoveCertain(12)
-	list.DisplayData()
+	list.RemoveCertain(12) // Data 12 not found
+	list.DisplayData()     // 2, 5, 9
 
 	fmt.Printf("Get data at index %d: ", 1)
-	fmt.Println(list.GetIndex(1))
+	fmt.Println(list.GetIndex(1)) // 5
+
 	fmt.Printf("Get data at index %d: ", 4)
-	fmt.Println(list.GetIndex(4))
+	fmt.Println(list.GetIndex(4)) // index 4 out of bounds exception
 }
