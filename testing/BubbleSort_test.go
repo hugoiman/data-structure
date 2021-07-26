044286f18fb6c4083883685f073694dd057733e0
Gen <hugogiman17@gmail.com>
2021-07-26 08:55:50 +0700

    Update testing

--- testing/BubbleSort_test.go
+++ testing/BubbleSort_test.go
@@ -3,11 +3,17 @@ package testing
 import (
 	bs "data-structure/BubbleSort"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
 func TestBubbleSort(t *testing.T) {
 	data := []int{3, 7, 2, 9, 4, 5}
+	result := bs.BubbleSort(data)
+	fmt.Println(result)
 
-	fmt.Println(bs.BubbleSort(data))
+	expected := []int{2, 3, 4, 5, 7, 9}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("\n got %v \n want %v \n given %v", result, expected, data)
+	}
 }
--- testing/SelectionSort_test.go
+++ testing/SelectionSort_test.go
@@ -3,11 +3,17 @@ package testing
 import (
 	bs "data-structure/SelectionSort"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
 func TestSelectionSort(t *testing.T) {
 	data := []int{3, 7, 2, 9, 4, 5}
+	result := bs.SelectionSort(data)
+	fmt.Println(result)
 
-	fmt.Println(bs.SelectionSort(data))
+	expected := []int{2, 3, 4, 5, 7, 9}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("\n got %v \n want %v \n given %v", result, expected, data)
+	}
 }
